f2c2e2553c02dc7420cfef43594d84c60b182a39
Shaikh_Abdullah <abdullah13112004@gmail.com>
2024-03-11 18:38:29 +0530

    Add: Some minor Change

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,23 +1,23 @@
-import logo from '../../assets/logo.svg'
-import Button from '../Button/Button'
+import logo from "../../assets/logo.svg";
+import Button from "../Button/Button";
 //import Hamicon from '../NavBar/hamburger'
-const Navbar=() =>{
+const Navbar = () => {
   return (
-    <div className='p-[2rem]'>
-        <div className='text-[#00FFD1] flex items-center'>
-        <div><img src={logo}></img></div>
-        <div className='flex gap-20 items-center [font-size:1rem] [font-weight:400] cursor-pointer'>
-            <div>Leaderboard</div>
-            <div>Sponsors</div>
-            <div>Projects</div>
-            <div>About Us</div>
-            <Button/>
+    <div className="p-[2rem]">
+      <div className="text-[#00FFD1] flex items-center">
+        <div>
+          <img src={logo}></img>
         </div>
-            
-
+        <div className="flex gap-20 items-center [font-size:1rem] [font-weight:400] cursor-pointer">
+          <div>Leaderboard</div>
+          <div>Sponsors</div>
+          <div>Projects</div>
+          <div>About Us</div>
+          <Button name={"Register"} />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
