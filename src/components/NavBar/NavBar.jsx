558ea40879f5a4365b46435f2d6c6b2bb74f90d5
Jagpreet Singh Khurana <119691733+Jagpreet153@users.noreply.github.com>
2024-03-12 23:39:22 +0530

    Add: final touch

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,42 +1,6 @@
 import { NavLink } from "react-router-dom";
 import logo from "../../assets/logo.svg";
 import Button from "../Button/Button";
-// //import Hamicon from '../NavBar/hamburger'
-// // import { useState } from "react";
-// // import { NavLink } from "react-router-dom";
-
-
-// // const [open,Isopen]=useState(false);
-// // const toggle=()=>{
-// //   Isopen(!open)
-// // }
-
-
-// const Navbar = () => {
-//   return (
-//     <div className="p-[2rem]">
-//       <div className="text-[#00FFD1] flex items-center justify-between md:flex">
-//         <div>
-//           <img src={logo}></img>
-//         </div>
-//         <div className="flex justify-end gap-12 items-center [font-size:1rem] [font-weight:400] cursor-pointer">
-//             <div>Sponsors</div> 
-//             <div>Sponsors</div> 
-//             <div>Sponsors</div> 
-//             <Button name={"Register"}/>
-//         </div>
-//       </div>
-//     </div>
-//   );
-
-
-  
-// };
-
-
-
-// export default Navbar;
-
 
 import  { useState } from 'react';
 
@@ -44,7 +8,7 @@ function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <nav className="p-4 overflow-hidden fixed w-full top-0 left-0 z-[1000] bg-bground">
+        <nav className="p-4 overflow-hidden fixed w-full top-0 left-0  bg-bground">
             <div className="max-w-7xl mx-auto px-4 pt-2">
                 <div className="flex justify-between items-center">
                     {/* Logo */}
