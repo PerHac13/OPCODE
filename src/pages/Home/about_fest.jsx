639b6a428ca6a93ba10d9cbd5bfc090dee9db1a9
Shaikh_Abdullah <abdullah13112004@gmail.com>
2024-03-13 18:43:40 +0530

    Fixed: Minor Bugs

--- src/pages/Home/about_fest.jsx
+++ src/pages/Home/about_fest.jsx
@@ -3,17 +3,20 @@ const About = () => {
     <div className="  p-[2rem] justify-center ">
       <div className="flex items-center justify-center">
         <div className="bg-[#00FFD1] w-[0.75rem] h-[0.75rem] [border-radius:100%]"></div>
-        <div className="text-[#fff] [font-size:2.5rem] p-[2rem]">
+        <div className="text-[#fff] [font-size:2.5rem] [text-align:center] p-[2rem] ">
           Know more about us
         </div>
       </div>
-      <span className="text-[#00ffd1] text-xl mt-3 justify-center flex [font-family: 'Calibrie']"> Hola Amigos!</span>
+      <span className="text-[#00ffd1] text-xl mt-3 justify-center flex [font-family: 'Calibrie']">
+        {" "}
+        Hola Amigos!
+      </span>
       <div className="text-[#c2ced2]  text-[1.15rem] [text-align:left] py-[1rem]">
         {" "}
-       Welcome to our very first Intra College Open Source
-        Fest! We're thrilled to embark on this exciting journey celebrating the
-        spirit of open collaboration, innovation, and community building. This
-        fest is a testament to the dedication and passion of our contributors,
+        Welcome to our very first Intra College Open Source Fest! We're thrilled
+        to embark on this exciting journey celebrating the spirit of open
+        collaboration, innovation, and community building. This fest is a
+        testament to the dedication and passion of our contributors,
         maintainers, and organizers who have worked tirelessly to make this
         event a reality. In the heart of this fest lies a vibrant showcase of
         projects, each representing the ingenuity and creativity of our diverse
@@ -22,7 +25,7 @@ const About = () => {
         array of interests and disciplines, reflecting the rich tapestry of
         talent within our college community. As you navigate through the
         projects section, you'll discover a treasure trove of ideas waiting to
-        be explored and contributions waiting to be made.  It's an opportunity
+        be explored and contributions waiting to be made. It's an opportunity
         for us to come together, learn from one another, and forge connections
         that will last far beyond the confines of this event. So let's dive in,
         engage with each other, and make this fest a truly unforgettable
@@ -33,9 +36,11 @@ const About = () => {
         can't wait to see where this journey takes us. Here's to a successful
         and inspiring Intra College Open Source Fest!
         <br></br>
-      
       </div>
-      <span className="text-[#00ffd1] text-xl mt-3 justify-center flex">  Vive la communauté open source ! (Long live the open source community!)</span>
+      <span className="text-[#00ffd1] text-xl mt-3 justify-center flex">
+        {" "}
+        Vive la communauté open source ! (Long live the open source community!)
+      </span>
     </div>
   );
 };
